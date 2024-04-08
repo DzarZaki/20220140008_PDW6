d6062780266007c3ebe6080959ed27f461fb57aa
DzarZaki <dzarzaki2@gmail.com>
2024-04-08 11:10:04 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+video.addEventListener("click", function video(){
+    alert("ini Video OP KNY S3");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio OP KNY S3");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar KNY S3");
+});
+
